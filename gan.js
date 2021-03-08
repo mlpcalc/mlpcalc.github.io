124c74e6ee88407eba66fedabe5a24989b2ac9b5
mlpcalc <mlpcalc@users.noreply.github.com>
2021-03-09 02:29:04 +0700

    Create gan.js

---
+++ gan.js
@@ -0,0 +1,8 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-VS85LB7P6F"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-VS85LB7P6F');
+</script>
