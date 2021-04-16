9adba672d2a098345bf3147900aa0e824b01c047
mlpcalc <mlpcalc@users.noreply.github.com>
2021-04-16 22:17:48 +0700

    Delete gan.js

--- gan.js
+++
@@ -1,8 +0,0 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-VS85LB7P6F"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-VS85LB7P6F');
-</script>
