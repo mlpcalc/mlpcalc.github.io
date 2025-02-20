34ec6e4cf95b067d2ed4a9014535b6201e955a27
ego-lay-atman-bay <70973578+ego-lay-atman-bay@users.noreply.github.com>
2025-02-20 15:56:47 -0700

    add "Dreams of Shire Lanka"

--- defaultEvent.js
+++ defaultEvent.js
@@ -1,9 +1,9 @@
 const eventsSelector = {
-	"siege": "A Mistmane New Year",
+	"siege": "Dreams of Shire Lanka",
 	"blitz": "Rise of the Cyber Ponies",
 	"blitz2": "Snow Shy and the Three Li'l Fillies",
 	//Default event type (0=siege, 1=blitz, 2= new blitz)
-	"eventType": 2
+	"eventType": 0
 }
 /*
 siege events:
@@ -76,7 +76,7 @@ A Mermare's Tale
 Generations - Dance Party!
 Tale of Tirek Part Three
 Abyssinian Adventure!
-
+Dreams of Shire Lanka
 
 
 
